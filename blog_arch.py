8c288b21a448c7f9025d0363232b514d64c7a56d
murilo ijanc <mbsd@m0x.ru>
2023-06-09 22:01:50 -0300

    chore: Initial commit

---
+++ blog_arch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023 Murilo Ijanc' <mbsd@m0x.ru>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from diagrams import Cluster, Diagram, Edge
+from diagrams.aws.general import Client, User
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.network import CF, CloudFrontEdgeLocation, Route53
+from diagrams.aws.security import ACM
+from diagrams.aws.storage import S3
+
+# output filename, default extension is png
+filename = "arch"
+
+
+def create_diagram():
+    with Diagram("blog architecture - microbio.rs", show=False, filename=filename):
+        user = User("User")
+        with Cluster("AWS Cloud"):
+            cf = CF("CloudFront")
+            acm = ACM("Cert *.microbio.rs")
+            lambda_edge = CloudFrontEdgeLocation("Security response\nheaders")
+            route = Route53("DNS microbio.rs")
+            s3 = S3("blog.microbio.rs")
+            user >> route >> acm >> cf >> s3
+            cf << lambda_edge
+
+
+if __name__ == "__main__":
+    create_diagram()
